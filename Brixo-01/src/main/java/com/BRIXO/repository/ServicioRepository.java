ee24b1a79360dfbb8f0619adf4283b6e4109de50
papidani1 <danilogc2001@gmail.com>
2026-04-08 20:07:36 -0500

    Remove all HTML template files and associated resources from the project, including navbar, home, index, login, service forms, user forms, and their respective lists. This cleanup is part of a larger refactor to streamline the front-end structure and improve maintainability.

--- Brixo-01/src/main/java/com/BRIXO/repository/ServicioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.BRIXO.repository;
-
-import com.BRIXO.model.Servicio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface ServicioRepository extends JpaRepository<Servicio, Long>, JpaSpecificationExecutor<Servicio> {
-}
