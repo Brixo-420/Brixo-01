494581f4a7973b320210c25e93fde6a1ac63efec
Edwin Mora <hector2mor64@gmail.com>
2026-04-08 19:32:57 -0500

    inicio

--- Brixo-01/src/main/java/com/BRIXO/controller/AuthController.java
+++ Brixo-01/src/main/java/com/BRIXO/controller/AuthController.java
@@ -3,6 +3,9 @@ package com.BRIXO.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
+
 @Controller
 public class AuthController {
 
